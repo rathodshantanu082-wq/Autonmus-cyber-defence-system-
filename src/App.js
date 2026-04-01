1e709f7ee344b5a10c5be307a38370d86123001c
PrasajBajpai <prasajbajpai12@gmail.com>
2026-04-01 21:39:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🛡️ AI Cyber Defense System</h1>
+
+      <p>
+        System Status:
+        <span style={{ color: "green", marginLeft: "10px" }}>
+          SECURE
+        </span>
+      </p>
+
+      <button>Scan Network</button>
+    </div>
+  );
+}
+
+export default App;
